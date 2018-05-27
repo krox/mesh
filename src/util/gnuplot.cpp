ed275612f72b85147a24403bec73be487a36292b
Simon Bürger <simon.buerger@rwth-aachen.de>
2018-05-27 15:25:43 +0200

    refactor gnuplot module a bit

--- src/util/gnuplot.cpp
+++ src/util/gnuplot.cpp
@@ -1,4 +1,6 @@
 #include "util/gnuplot.h"
+
+#include <fmt/format.h>
 #include <fstream>
 
 int Gnuplot::nplotsGlobal = 0;
@@ -12,60 +14,73 @@ Gnuplot::Gnuplot(bool persist) : plotID(nplotsGlobal++)
 	if (pipe == nullptr)
 		throw "ERROR: Could not open gnuplot (is it installed?)";
 
-	cmd("set output\n");
-	cmd("set terminal x11\n");
-}
-
-void Gnuplot::cmd(const std::string &s)
-{
-	assert(s.back() == '\n');
-	fputs(s.c_str(), pipe);
+	fmt::print(pipe, "set output\n");
+	fmt::print(pipe, "set terminal x11\n");
 	fflush(pipe);
 }
 
-void Gnuplot::cmd(const boost::basic_format<char> &fmt) { cmd(fmt.str()); }
-
-void Gnuplot::plotFunction(const std::string &fun, const std::string &title)
+Gnuplot &Gnuplot::plotFunction(const std::string &fun, const std::string &title)
 {
-	cmd(format("%s %s title \"%s\"\n") % (nplots ? "replot" : "plot") % fun %
-	    (title.size() ? title : fun));
+	fmt::print(pipe, "{} {} title \"{}\"\n", nplots ? "replot" : "plot", fun,
+	           (title.size() ? title : fun));
+	fflush(pipe);
 	++nplots;
+	return *this;
 }
 
-void Gnuplot::plotData(const std::vector<double> &ys, const std::string &title)
+Gnuplot &Gnuplot::plotFunction(const std::function<double(double)> &fun,
+                               double a, double b, const std::string &title)
 {
-	std::string filename =
-	    (format("gnuplot_%s_%s.txt") % plotID % nplots).str();
+	auto oldStyle = style;
+	style = "lines";
+	std::vector<double> xs, ys;
+	for (int i = 0; i <= 100; ++i)
+	{
+		xs.push_back(a + (b - a) * i / 100);
+		ys.push_back(fun(xs.back()));
+	}
+	plotData(xs, ys, title);
+	style = oldStyle;
+	return *this;
+}
+
+Gnuplot &Gnuplot::plotData(const std::vector<double> &ys,
+                           const std::string &title)
+{
+	std::string filename = fmt::format("gnuplot_{}_{}.txt", plotID, nplots);
 	std::ofstream file(filename);
 	for (size_t i = 0; i < ys.size(); ++i)
 		file << i << " " << ys[i] << "\n";
 	file.flush();
 	file.close();
-	cmd(format("%s '%s' using 1:2 with %s title \"%s\"\n") %
-	    (nplots ? "replot" : "plot") % filename % style % title);
+	fmt::print(pipe, "{} '{}' using 1:2 with {} title \"{}\"\n",
+	           (nplots ? "replot" : "plot"), filename, style, title);
+	fflush(pipe);
 	++nplots;
+	return *this;
 }
 
-void Gnuplot::plotData(const std::vector<double> &xs,
-                       const std::vector<double> &ys, const std::string &title)
+Gnuplot &Gnuplot::plotData(const std::vector<double> &xs,
+                           const std::vector<double> &ys,
+                           const std::string &title)
 {
-	std::string filename =
-	    (format("gnuplot_%s_%s.txt") % plotID % nplots).str();
+	std::string filename = fmt::format("gnuplot_{}_{}.txt", plotID, nplots);
 	std::ofstream file(filename);
 	assert(xs.size() == ys.size());
 	for (size_t i = 0; i < xs.size(); ++i)
 		file << xs[i] << " " << ys[i] << "\n";
 	file.flush();
 	file.close();
-	cmd(format("%s '%s' using 1:2 with %s title \"%s\"\n") %
-	    (nplots ? "replot" : "plot") % filename % style % title);
+	fmt::print(pipe, "{} '{}' using 1:2 with {} title \"{}\"\n",
+	           (nplots ? "replot" : "plot"), filename, style, title);
+	fflush(pipe);
 	++nplots;
+	return *this;
 }
 
-void Gnuplot::plotHistogram(const histogram &hist, const std::string &title)
+Gnuplot &Gnuplot::plotHistogram(const histogram &hist, const std::string &title)
 {
-	std::string filename =
-	    (format("gnuplot_%s_%s.txt") % plotID % nplots).str();
+	std::string filename = fmt::format("gnuplot_{}_{}.txt", plotID, nplots);
 	std::ofstream file(filename);
 
 	for (size_t i = 0; i < hist.bins.size(); ++i)
@@ -73,32 +88,59 @@ void Gnuplot::plotHistogram(const histogram &hist, const std::string &title)
 		     << "\n";
 	file.flush();
 	file.close();
-	cmd(format("%s '%s' using 1:2 with points title \"%s\"\n") %
-	    (nplots ? "replot" : "plot") % filename % title);
+	fmt::print(pipe, "{} '{}' using 1:2 with points title \"{}\"\n",
+	           (nplots ? "replot" : "plot"), filename, title);
+	fflush(pipe);
 	nplots++;
+	return *this;
 }
 
-void Gnuplot::setRangeX(double min, double max)
+Gnuplot &Gnuplot::setRangeX(double min, double max)
 {
-	cmd(format("set xrange[%s : %s]\n") % min % max);
+	fmt::print(pipe, "set xrange[{} : {}]\n", min, max);
+	fflush(pipe);
+	return *this;
 }
 
-void Gnuplot::setRangeY(double min, double max)
+Gnuplot &Gnuplot::setRangeY(double min, double max)
 {
-	cmd(format("set yrange[%s : %s]\n") % min % max);
+	fmt::print(pipe, "set yrange[{} : {}]\n", min, max);
+	fflush(pipe);
+	return *this;
 }
 
-void Gnuplot::setRangeZ(double min, double max)
+Gnuplot &Gnuplot::setRangeZ(double min, double max)
 {
-	cmd(format("set zrange[%s : %s]\n") % min % max);
+	fmt::print(pipe, "set zrange[{} : {}]\n", min, max);
+	fflush(pipe);
+	return *this;
 }
 
-void Gnuplot::setLogScaleX() { cmd("set logscale x\n"); }
-void Gnuplot::setLogScaleY() { cmd("set logscale y\n"); }
-void Gnuplot::setLogScaleZ() { cmd("set logscale z\n"); }
-
-void Gnuplot::clear()
+Gnuplot &Gnuplot::setLogScaleX()
 {
-	cmd("clear\n");
+	fmt::print(pipe, "set logscale x\n");
+	fflush(pipe);
+	return *this;
+}
+
+Gnuplot &Gnuplot::setLogScaleY()
+{
+	fmt::print(pipe, "set logscale y\n");
+	fflush(pipe);
+	return *this;
+}
+
+Gnuplot &Gnuplot::setLogScaleZ()
+{
+	fmt::print(pipe, "set logscale z\n");
+	fflush(pipe);
+	return *this;
+}
+
+Gnuplot &Gnuplot::clear()
+{
+	fmt::print(pipe, "clear\n");
+	fflush(pipe);
 	nplots = 0;
+	return *this;
 }
--- src/util/gnuplot.h
+++ src/util/gnuplot.h
@@ -1,7 +1,8 @@
 #ifndef MESH_GNUPLOT_H
 #define MESH_GNUPLOT_H
 
-#include "boost/format.hpp"
+#include <functional>
+
 #include "util/stats.h"
 
 class Gnuplot
@@ -10,7 +11,6 @@ class Gnuplot
 	static int nplotsGlobal;
 	int nplots = 0;
 	const int plotID;
-	using format = boost::format;
 
   public:
 	std::string style = "points";
@@ -18,58 +18,39 @@ class Gnuplot
 	/** constructor */
 	explicit Gnuplot(bool persist = true);
 
-	/** send a command to gnuplot */
-	void cmd(const std::string &s);
-	void cmd(const boost::basic_format<char> &fmt);
-
 	/** plot a function given by a string that gnuplot can understand */
-	void plotFunction(const std::string &fun, const std::string &title = "");
+	Gnuplot &plotFunction(const std::string &fun,
+	                      const std::string &title = "");
 
-	/** plot a function given as Functor */
-	template <typename F>
-	void plotFunction(F f, double min, double max,
-	                  const std::string &title = "");
+	/** plot a function given as std::function object */
+	Gnuplot &plotFunction(const std::function<double(double)> &fun, double min,
+	                      double max, const std::string &title = "");
 
 	/** plot raw data points (i, ys[i]) */
-	void plotData(const std::vector<double> &ys,
-	              const std::string &title = "data");
+	Gnuplot &plotData(const std::vector<double> &ys,
+	                  const std::string &title = "data");
 
 	/** plot raw data points (xs[i], ys[i]) */
-	void plotData(const std::vector<double> &xs, const std::vector<double> &ys,
-	              const std::string &title = "data");
+	Gnuplot &plotData(const std::vector<double> &xs,
+	                  const std::vector<double> &ys,
+	                  const std::string &title = "data");
 
 	/** plot a histogram */
-	void plotHistogram(const histogram &hist,
-	                   const std::string &title = "hist");
+	Gnuplot &plotHistogram(const histogram &hist,
+	                       const std::string &title = "hist");
 
 	/** set range of plot */
-	void setRangeX(double min, double max);
-	void setRangeY(double min, double max);
-	void setRangeZ(double min, double max);
+	Gnuplot &setRangeX(double min, double max);
+	Gnuplot &setRangeY(double min, double max);
+	Gnuplot &setRangeZ(double min, double max);
 
 	/** make the plot logarithmic */
-	void setLogScaleX();
-	void setLogScaleY();
-	void setLogScaleZ();
+	Gnuplot &setLogScaleX();
+	Gnuplot &setLogScaleY();
+	Gnuplot &setLogScaleZ();
 
 	/** remove all plots (but keep settings) */
-	void clear();
+	Gnuplot &clear();
 };
-
-template <typename F>
-inline void Gnuplot::plotFunction(F f, double a, double b,
-                                  const std::string &title)
-{
-	auto oldStyle = style;
-	style = "lines";
-	std::vector<double> xs, ys;
-	for (int i = 0; i <= 100; ++i)
-	{
-		xs.push_back(a + (b - a) * i / 100);
-		ys.push_back(f(xs.back()));
-	}
-	plotData(xs, ys, title);
-	style = oldStyle;
-}
 
 #endif
