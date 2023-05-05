678234b93baf1cf2a670f3e0f8bf644830470e28
Simon Bürger <simon.buerger@rwth-aachen.de>
2023-05-05 18:18:59 +0100

    more serious memory testing to get Lattice on solid grounds

--- src/apps/ompbench.cpp
+++ src/apps/ompbench.cpp
@@ -3,6 +3,7 @@
 #include "groups/su2.h"
 #include "groups/sun.h"
 #include "groups/u1.h"
+#include "lattice/lattice.h"
 #include "lattice/tensor.h"
 #include "util/memory.h"
 #include "util/random.h"
@@ -11,22 +12,189 @@
 
 using namespace mesh;
 
-template <class T> using SU7 = SUN<T, 7>;
-template <class T> using SU8 = SUN<T, 8>;
-template <class T> using SU9 = SUN<T, 9>;
-template <class T> using SU10 = SUN<T, 10>;
-
 // prevent compiler optimizations by doing nothing but pretending to to anything
 void escape(void *p) { asm volatile("" : : "g"(p) : "memory"); }
 void clobber() { asm volatile("" : : : "memory"); }
 
-// size of objects (in bytes) to benchmark.
-static constexpr size_t buffer_size = 1024 * 1024 * 128;
-static constexpr int niter = 20;
+// standardized output to make the log nicely human readable.
+void print_line(std::string_view name, double secs, double bytes,
+                double flops = 0.0 / 0.0)
+{
+	fmt::print(
+	    "{:<25}: {:5.2f} secs, {:8.3f} GiB, {:8.3f} GiB/s, {:8.3f} GFlops/s\n",
+	    name, secs, bytes / 1024. / 1024. / 1024.,
+	    bytes / secs / 1024. / 1024. / 1024, flops / secs * 1e-9);
+}
 
-// tiny buffer, many iterations -> everything in cache should hit compute limit
-// static constexpr size_t buffer_size = 1;
-// static constexpr int niter = 2'000'000;
+// create a grid of a size such that a lattice of type T will have an
+// appropriate size for benchmarking
+template <class T> Grid get_grid()
+{
+	for (int L : {8, 12, 16, 24, 32, 48, 64, 96, 128, 192, 256})
+		if (size_t(L) * L * L * L * sizeof(T) >= size_t(512) * 1024 * 1024)
+			return Grid({L, L, L, L});
+	assert(false);
+}
+
+// benchmark `f` by running it `niter` times. returns seconds per iteration
+template <class F> double benchmark(F f, int niter = 10, int nwarmup = 5)
+{
+	util::Stopwatch sw;
+	for (int iter = -nwarmup; iter < niter; ++iter)
+	{
+		if (iter == 0)
+			sw.start();
+		f();
+	}
+	sw.stop();
+	return sw.secs() / niter;
+}
+
+// direct memory benchmarks with very little arithmetic
+void bench_memory()
+{
+	fmt::print("\n==================== memory ====================\n");
+
+	using T = float;
+	auto grid = get_grid<T>();
+	auto a = Lattice<T>::zeros(grid);
+	auto b = Lattice<T>::zeros(grid);
+	double secs;
+
+	// read
+
+	secs = benchmark([&a]() {
+		escape(a.data());
+		auto s = sum(a);
+		escape(&s);
+	});
+	print_line("sum", secs, a.bytes());
+
+	// write
+
+	secs = benchmark([&a]() {
+		escape(a.data());
+		std::memset(a.data(), 0, a.bytes());
+		escape(a.data());
+	});
+	print_line("std::memset (1 thread)", secs, a.bytes());
+
+	secs = benchmark([&a]() {
+		escape(a.data());
+		a.fill_zeros();
+		escape(a.data());
+	});
+	print_line("fill_zeros", secs, a.bytes());
+
+	// copy
+
+	secs = benchmark([&a, &b]() {
+		escape(a.data());
+		std::memcpy(b.data(), a.data(), a.bytes());
+		escape(b.data());
+	});
+	print_line("std::memcpy (1 thread)", secs, a.bytes() + b.bytes());
+
+	secs = benchmark([&a, &b]() {
+		escape(a.data());
+		b = a;
+		escape(b.data());
+	});
+	print_line("operator=", secs, a.bytes() + b.bytes());
+
+	secs = benchmark([&a, &b]() {
+		escape(a.data());
+		lattice_apply([](auto &x, auto const &y) { x = y; }, b, a);
+		escape(b.data());
+	});
+
+	print_line("lattice_apply('=')", secs, a.bytes() + b.bytes());
+}
+
+// lattice-parallel arithmetic (mostly memory bound)
+void bench_axpy()
+{
+	fmt::print("\n==================== axpy ====================\n");
+
+	auto run = []<class T>(std::string_view name, int complexity) {
+		auto grid = get_grid<T>();
+
+		auto a = Lattice<T>::zeros(grid);
+		auto b = Lattice<T>::zeros(grid);
+		auto c = Lattice<T>::zeros(grid);
+
+		util::Stopwatch sw;
+		int niter = 10;
+
+		for (int iter = -5; iter < niter; ++iter)
+		{
+			if (iter == 0)
+				sw.start();
+			escape(b.data());
+			escape(c.data());
+			add_mul(a, T(1.234), c);
+			escape(a.data());
+		}
+		sw.stop();
+		double vol = a.grid().size();
+
+		print_line(name, sw.secs() / niter, a.bytes() + b.bytes() + c.bytes(),
+		           vol * complexity);
+	};
+
+	run.operator()<float>("float", 1);
+	run.operator()<util::complex<float>>("complex<float>", 4);
+	run.operator()<util::Matrix<util::complex<float>, 3>>("SU3<float>",
+	                                                      3 * 3 * 3 * 4);
+	run.operator()<util::Matrix<util::complex<float>, 4>>("SU4<float>",
+	                                                      4 * 4 * 4 * 4);
+	/*run.operator()<SU5<float>>("SU5<float>", 5 * 5 * 5 * 4);
+	run.operator()<SU6<float>>("SU6<float>", 6 * 6 * 6 * 4);
+	run.operator()<util::simd<float>>("simd<float>", util::simd<float>::size());
+
+	run.operator()<SU3<double>>("SU3<double>", 3 * 3 * 3 * 4);
+	run.operator()<SU4<double>>("SU4<double>", 4 * 4 * 4 * 4);
+	run.operator()<SU5<double>>("SU5<double>", 5 * 5 * 5 * 4);
+	run.operator()<SU6<double>>("SU6<double>", 6 * 6 * 6 * 4);
+	run.operator()<double>("double", 1);
+	run.operator()<util::complex<double>>("complex<double>", 4);
+	run.operator()<util::simd<double>>("simd<double>",
+	                                   util::simd<double>::size());*/
+}
+
+#if 0
+template <class G> void bench_exp(std::string const &name)
+{
+	fmt::print("\n==================== exponential ====================\n");
+
+	auto rng = util::xoshiro256("bench_exp");
+	auto a = G::randomAlgebraElement(rng);
+	G b;
+
+	int order = 12;
+	util::Stopwatch sw;
+	int niter = 1'000'000;
+
+	for (int iter = -2; iter < niter; ++iter)
+	{
+		if (iter == 0)
+			sw.start();
+		clobber();
+		b = exp(a);
+		clobber();
+	}
+	sw.stop();
+
+	double flops = order * TensorTraits<G>::flops_mul;
+	double bytes = 2.0 * sizeof(G);
+
+	/*fmt::print("group={}, flops_mul={}, sizeof(G)={}\n", G::name(),
+	           TensorTraits<G>::flops_mul, sizeof(G));*/
+	fmt::print(
+	    "{:<25}: {:5.2f} secs, {:8.3f} GiB, {:8.3f} GiB/s, {:8.3f} GFlops/s\n",
+	    name, sw.secs(), bytes * 1e-9, bytes * 1e-9 * niter / sw.secs(),
+	    flops * niter / sw.secs() * 1e-9);
+}
 
 template <class G> void bench(std::string const &name)
 {
@@ -76,13 +244,16 @@ template <template <class> class G> void benchGroup(std::string const &name)
 	// bench<G<util::simd<float>>>(fmt::format("{}, single, simd", name));
 	bench<G<util::simd<double>>>(fmt::format("{}, double, simd", name));
 }
+#endif
 
 int main()
 {
 	util::Stopwatch sw;
 	sw.start();
 
-	benchGroup<U1>("U1");
+	bench_memory();
+	bench_axpy();
+	/*benchGroup<U1>("U1");
 	benchGroup<SU2>("SU2");
 	benchGroup<SU3>("SU3");
 	benchGroup<SU4>("SU4");
@@ -91,7 +262,12 @@ int main()
 	benchGroup<SU7>("SU7");
 	benchGroup<SU8>("SU8");
 	benchGroup<SU9>("SU9");
-	benchGroup<SU10>("SU10");
+	benchGroup<SU10>("SU10");*/
+
+	/*bench_exp<SU3<float>>("SU3, single");
+	bench_exp<SU3<double>>("SU3, double");
+	bench_exp<SU3<util::simd<float>>>("SU3, single, simd");
+	bench_exp<SU3<util::simd<double>>>("SU3, double, simd");*/
 
 	sw.stop();
 	fmt::print("total wall time = {:.2} seconds\n", sw.secs());
--- src/lattice/lattice.h
+++ src/lattice/lattice.h
@@ -11,12 +11,16 @@
 #include <array>
 #include <cassert>
 #include <cstring>
+#include <omp.h>
 #include <type_traits>
 
 namespace mesh {
 
 inline int64_t latticeAllocCount = 0;
 inline util::Stopwatch swCshift;
+
+void parallel_memcpy(void *, void const *, size_t);
+void parallel_memset(void *, int, size_t);
 
 template <typename T> class Lattice
 {
@@ -27,6 +31,8 @@ template <typename T> class Lattice
 	// static_assert(sizeof(Object) == Object::size() * sizeof(Real));
 	// static_assert(std::is_same_v<Real, float> || std::is_same_v<Real,
 	// double>);
+
+	using value_type = T;
 
   private:
 	Grid grid_ = {}; // this is dimension 1, size 0
@@ -47,13 +53,17 @@ template <typename T> class Lattice
 	static Lattice zeros(Grid const &g)
 	{
 		auto a = Lattice(g);
-		std::memset(a.data(), 0, g.size() * sizeof(T));
+		a.fill_zeros();
 		return a;
 	}
+
+	void fill_zeros() { parallel_memset(data(), 0, bytes()); }
 
 	Grid const &grid() const { return grid_; }
 	T *data() { return data_.data(); }
 	T const *data() const { return data_.data(); }
+
+	size_t bytes() const { return data_.size() * sizeof(T); }
 
 	// copy/move operations
 	Lattice(Lattice const &other)
@@ -61,7 +71,7 @@ template <typename T> class Lattice
 	      data_(util::make_aligned_unique_span<T>(grid().size()))
 	{
 		++latticeAllocCount;
-		std::memcpy(data(), other.data(), grid().size() * sizeof(T));
+		parallel_memcpy(data(), other.data(), grid().size() * sizeof(T));
 	}
 
 	Lattice(Lattice &&other) noexcept
@@ -77,7 +87,9 @@ template <typename T> class Lattice
 			data_ = util::make_aligned_unique_span<T>(other.grid().size());
 		}
 		grid_ = other.grid();
-		std::memcpy(data(), other.data(), grid().size() * sizeof(T));
+
+		parallel_memcpy(data(), other.data(), grid().size() * sizeof(T));
+
 		return *this;
 	}
 
@@ -159,6 +171,17 @@ template <typename T> class LatticeStack
 	Lattice<T> const &operator[](size_t i) const { return data_.at(i); }
 };
 
+template <class T> struct is_lattice : std::integral_constant<bool, false>
+{};
+template <class T>
+struct is_lattice<Lattice<T>> : std::integral_constant<bool, true>
+{};
+template <class T>
+struct is_lattice<LatticeStack<T>> : std::integral_constant<bool, true>
+{};
+
+template <class T> inline constexpr bool is_lattice_v = is_lattice<T>::value;
+
 /*
 // store LatticeStack to file as [Nd,X,Y,Z,T,ncomp] array
 //     * returns resulting dataset
@@ -214,10 +237,7 @@ void lattice_apply(F f, Lattice<T> &a, Lattice<Ts> const &...as)
 	assert(((a.grid() == as.grid()) && ...));
 	size_t size = a.grid().size();
 
-	// TODO: in principle, this should be the only place in the code where we
-	//       do explicit multithreadig using OMP. Though some careful tests
-	//       are in order before activating it
-	// #pragma omp parallel for schedule(static)
+#pragma omp parallel for schedule(static)
 	for (size_t i = 0; i < size; ++i)
 		f(a.data()[i], as.data()[i]...);
 }
@@ -228,6 +248,57 @@ void lattice_apply(F f, LatticeStack<T> &a, LatticeStack<Ts> const &...as)
 	assert(((a.size() == as.size()) && ...));
 	for (size_t i = 0; i < a.size(); ++i)
 		lattice_apply(f, a[i], as[i]...);
+}
+
+template <typename F, typename T, typename... Ts>
+auto lattice_sum(F f, Lattice<T> const &a, Lattice<Ts> const &...as)
+    -> decltype(f(a.data()[0], as.data()[0]...))
+{
+	assert(((a.grid() == as.grid()) && ...));
+
+	using R = decltype(f(a.data()[0], as.data()[0]...));
+	auto r = R(0);
+
+	// number of local accumulators:
+	//   1) improve numerics (similar to "pair summation")
+	//   2) improve instruction-level parallelism
+	//   3) increases register pressure, so we scale it by the size of T
+	//   4) the number '64' is kinda arbitrary
+	constexpr size_t naccs =
+	    std::max(size_t(1), std::bit_floor(size_t(64) / sizeof(T)));
+
+	size_t size = a.grid().size();
+	assert(size % naccs == 0);
+
+#pragma omp parallel
+	{
+		R accs[naccs];
+		for (size_t k = 0; k < naccs; ++k)
+			accs[k] = R(0);
+
+#pragma omp for schedule(static) nowait
+		for (size_t i = 0; i < size / naccs; ++i)
+			for (size_t k = 0; k < naccs; ++k)
+				accs[k] +=
+				    f(a.data()[i * naccs + k], as.data()[i * naccs + k]...);
+
+		for (size_t k = 1; k < naccs; ++k)
+			accs[0] += accs[k];
+
+#pragma omp critical
+		r += accs[0];
+	}
+	return r;
+}
+
+template <typename F, typename T, typename... Ts>
+auto lattice_sum(F f, LatticeStack<T> const &a, LatticeStack<Ts> const &...as)
+    -> decltype(f(a[0].data()[0], as[0].data()[0]...))
+{
+	auto r = decltype(f(a[0].data()[0], as[0].data()[0]...))(0);
+	for (size_t i = 0; i < a.size(); ++i)
+		r += lattice_sum(f, a[i], as[i]...);
+	return r;
 }
 
 #define UTIL_DEFINE_LATTICE_BINARY(op)                                         \
@@ -327,18 +398,12 @@ void lattice_apply(F f, LatticeStack<T> &a, LatticeStack<Ts> const &...as)
 	template <typename T>                                                      \
 	auto name(Lattice<T> const &a)->decltype(fun(std::declval<T>()))           \
 	{                                                                          \
-		decltype(fun(std::declval<T>())) s = {};                               \
-		for (size_t i = 0; i < a.grid().size(); ++i)                           \
-			s += fun(a.data()[i]);                                             \
-		return s;                                                              \
+		return lattice_sum([](T const &aa) { return fun(aa); }, a);            \
 	}                                                                          \
 	template <typename T>                                                      \
 	auto name(LatticeStack<T> const &a)->decltype(fun(std::declval<T>()))      \
 	{                                                                          \
-		decltype(fun(std::declval<T>())) s = {};                               \
-		for (size_t i = 0; i < a.size(); ++i)                                  \
-			s += name(a[i]);                                                   \
-		return s;                                                              \
+		return lattice_sum([](T const &aa) { return fun(aa); }, a);            \
 	}
 
 UTIL_DEFINE_LATTICE_BINARY(+)
@@ -350,7 +415,11 @@ UTIL_DEFINE_LATTICE_UNARY(adj)
 UTIL_DEFINE_LATTICE_UNARY(exp)
 UTIL_DEFINE_LATTICE_UNARY(projectOnAlgebra)
 
-UTIL_DEFINE_LATTICE_REDUCTION(sum, sum)
+struct my_identity
+{
+	template <class T> T operator()(T const &x) { return x; }
+};
+UTIL_DEFINE_LATTICE_REDUCTION(sum, my_identity{})
 UTIL_DEFINE_LATTICE_REDUCTION(norm2, norm2)
 UTIL_DEFINE_LATTICE_REDUCTION(sumTrace, trace)
 
@@ -371,6 +440,14 @@ template <typename T> Lattice<T> operator*(Lattice<T> &&a, double b)
 	for (size_t i = 0; i < a.grid().size(); ++i)
 		a.data()[i] *= b;
 	return std::move(a);
+}
+
+// a += b*c
+template <class A, class B, class C>
+    requires(!is_lattice_v<B>)
+void add_mul(Lattice<A> &a, B b, Lattice<C> const &c)
+{
+	lattice_apply([b](A &aa, C const &cc) { aa += b * cc; }, a, c);
 }
 
 template <typename T>
@@ -411,6 +488,43 @@ Lattice<T> cshift(Lattice<T> const &a, int dir, int offset)
 	}
 
 	return r;
+}
+
+// Same as std::memcpy, put parallelized using OMP
+inline void parallel_memcpy(void *dest, void const *src, size_t count)
+{
+#pragma omp parallel
+	{
+		int rank = omp_get_thread_num();
+		int nranks = omp_get_num_threads();
+		size_t chunk = (count / nranks) & -4096;
+
+		void *my_dest = (char *)dest + rank * chunk;
+		void const *my_src = (char const *)src + rank * chunk;
+
+		size_t my_size =
+		    (rank == nranks - 1) ? count - (nranks - 1) * chunk : chunk;
+
+		std::memcpy(my_dest, my_src, my_size);
+	}
+}
+
+// Same as std::memset, put parallelized using OMP
+inline void parallel_memset(void *dest, int ch, std::size_t count)
+{
+#pragma omp parallel
+	{
+		int rank = omp_get_thread_num();
+		int nranks = omp_get_num_threads();
+		size_t chunk = (count / nranks) & -4096;
+
+		void *my_dest = (char *)dest + rank * chunk;
+
+		size_t my_size =
+		    (rank == nranks - 1) ? count - (nranks - 1) * chunk : chunk;
+
+		std::memset(my_dest, ch, my_size);
+	}
 }
 
 } // namespace mesh
